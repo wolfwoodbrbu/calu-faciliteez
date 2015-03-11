46c50b0e3320b79b7f20a5c4c9689c60a92a6e0f
FayeBickerton <fayebickerton@gmail.com>
2015-03-11 18:59:12 -0400

    Made the Temperature and Humidity calculations more accurate.
    Starting to add a way to switch between different Temperature Scales
    like F and C. May jusr have to do this on the ThingWorx side so Alerts
    can be persistant.

--- phidget1125thing/src/main/java/com/calu/faciliteez/phidget1125thing/SensorThing.java
+++ phidget1125thing/src/main/java/com/calu/faciliteez/phidget1125thing/SensorThing.java
@@ -1,5 +1,8 @@
 package com.calu.faciliteez.phidget1125thing;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,18 +12,56 @@ import com.thingworx.communications.client.ConnectedThingClient;
 import com.thingworx.communications.client.things.VirtualThing;
 import com.thingworx.metadata.annotations.ThingworxPropertyDefinition;
 import com.thingworx.metadata.annotations.ThingworxPropertyDefinitions;
+import com.thingworx.types.properties.Property;
+import com.thingworx.types.properties.collections.PendingPropertyUpdatesByProperty;
 
 @ThingworxPropertyDefinitions(
 
 properties = {
-		@ThingworxPropertyDefinition(name = "P1125_Temp", description = "The current temperature of the Phidget 1125 sensor", baseType = "NUMBER", aspects = {
-				"dataChangeType:ALWAYS", "dataChangeThreshold:0",
-				"cacheTime:0", "isPersistent:FALSE", "isReadOnly:TRUE",
-				"pushType:VALUE", "defaultValue:0" }),
-		@ThingworxPropertyDefinition(name = "P1125_RH", description = "The current relative humidity of the Phidget 1125 sensor", baseType = "NUMBER", aspects = {
-				"dataChangeType:ALWAYS", "dataChangeThreshold:0",
-				"cacheTime:0", "isPersistent:FALSE", "isReadOnly:TRUE",
-				"pushType:VALUE", "defaultValue:0" }), })
+		@ThingworxPropertyDefinition(
+				name = "P1125_Temp", 
+				description = "The current temperature of the Phidget 1125 sensor", 
+				baseType = "NUMBER", 
+				aspects = {
+						"dataChangeType:ALWAYS", 
+						"dataChangeThreshold:0",
+						"cacheTime:0", 
+						"isPersistent:FALSE", 
+						"isReadOnly:TRUE",
+						"pushType:VALUE", 
+						"defaultValue:0" 
+						}
+				),
+		@ThingworxPropertyDefinition(
+				name = "P1125_RH", 
+				description = "The current relative humidity of the Phidget 1125 sensor", 
+				baseType = "NUMBER", 
+				aspects = {
+						"dataChangeType:ALWAYS", 
+						"dataChangeThreshold:0",
+						"cacheTime:0", 
+						"isPersistent:FALSE", 
+						"isReadOnly:TRUE",
+						"pushType:VALUE", 
+						"defaultValue:0" 
+						}
+				), 
+		@ThingworxPropertyDefinition(
+				name = "P1125_TempScale", 
+				description = "Which Temperature Scale do we want.", 
+				baseType = "STRING", 
+				aspects = {
+						"dataChangeType:ALWAYS", 
+						"dataChangeThreshold:0",
+						"cacheTime:0", 
+						"isPersistent:TRUE", 
+						"isReadOnly:FALSE",
+						"pushType:VALUE", 
+						"defaultValue:F" 
+						}
+				),
+})
+
 public class SensorThing extends VirtualThing {
 
 	/**
@@ -54,6 +95,7 @@ public class SensorThing extends VirtualThing {
 		try {
 			setDefaultPropertyValue("P1125_Temp");
 			setDefaultPropertyValue("P1125_RH");
+			setDefaultPropertyValue("P1125_TempScale");
 		} catch (Exception localException) {
 			LOG.error("Failed to set default value.", localException);
 		}
@@ -81,8 +123,12 @@ public class SensorThing extends VirtualThing {
 	@Override
 	public void processScanRequest() throws Exception {
 		super.processScanRequest();
-
-		Double currentTemperatureF = getTemperature();
+		
+		PendingPropertyUpdatesByProperty pu = getPendingPropertyUpdates();
+		Property Scale = getProperty("P1125_TempScale");
+		String TempScale = Scale.getValue().getStringValue();
+		LOG.debug("P1125_TempScale = " + TempScale);
+		Double currentTemperatureF = getTemperature(TempScale);
 		Double currentHumidity = getHumidity();
 		LOG.debug("P1125_Temp" + "=" + currentTemperatureF);
 		LOG.debug("P1125_RH" + "=" + currentHumidity);
@@ -93,52 +139,65 @@ public class SensorThing extends VirtualThing {
 	}
 
 	private Double getHumidity() {
-		final Double HUMIDITY_TRANLATE_1 = 0.1906;
-		final Double HUMIDITY_TRANLATE_2 = 40.2;
-		Double humidity = 0.00;
+		final BigDecimal HUMIDITY_TRANLATE_1 = new BigDecimal(0.1906);
+		final BigDecimal HUMIDITY_TRANLATE_2 = new BigDecimal(40.2);
+		int sensorValue = 0;
+		BigDecimal humidity = new BigDecimal(0);
 
 		if (simulated != null && simulated.equals("simulated")) {
-			humidity = 20.5;
+			sensorValue = 319;
 		} else {
-			try {
-				humidity = (ik.getSensorValue(1) * HUMIDITY_TRANLATE_1)
-						- HUMIDITY_TRANLATE_2;
-			} catch (PhidgetException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			sensorValue = getSensorValue(0);
 		}
-		humidity = humidity * 100;
-		Long l = Math.round(humidity);
-		humidity = l.doubleValue();
-		humidity = humidity / 100;
-		
-		return humidity;
+		humidity = new BigDecimal(sensorValue);
+
+		humidity = humidity.multiply(HUMIDITY_TRANLATE_1);
+		humidity = humidity.subtract(HUMIDITY_TRANLATE_2);
+
+		humidity = round(humidity, 2);
+
+		return humidity.doubleValue();
 	}
 
-	private Double getTemperature() {
-		final double TEMPERATURE_TRANSLATE_1 = 0.22222;
-		final double TEMPERATURE_TRANSLATE_2 = 61.11;
-		Double temperature = 0.00;
+	private Double getTemperature(String Scale) {
+		final BigDecimal TEMPERATURE_TRANSLATE_1 = new BigDecimal(0.22222);
+		final BigDecimal TEMPERATURE_TRANSLATE_2 = new BigDecimal(61.11);
+		int sensorValue = 0;
+		BigDecimal temperature = new BigDecimal(0);
 
 		if (simulated != null && simulated.equals("simulated")) {
-			temperature = 70.5;
+			sensorValue = 319;
 		} else {
-			try {
-				temperature = (ik.getSensorValue(1) * TEMPERATURE_TRANSLATE_1)
-						- TEMPERATURE_TRANSLATE_2;
-				temperature = (temperature * 9 / 5) + 32;
-			} catch (PhidgetException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			sensorValue = getSensorValue(1);
 		}
-		temperature = temperature * 100;
-		Long l = Math.round(temperature);
-		temperature = l.doubleValue();
-		temperature = temperature / 100;
+		temperature = new BigDecimal(sensorValue);
+
+		temperature = temperature.multiply(TEMPERATURE_TRANSLATE_1);
+		temperature = temperature.subtract(TEMPERATURE_TRANSLATE_2);
+
+		if (Scale.equals("F")) {
+			temperature = temperature.multiply(new BigDecimal(9));
+			temperature = temperature.divide(new BigDecimal(5));
+			temperature = temperature.add(new BigDecimal(32));
+		}
 		
-		return temperature;
+		
+		temperature = round(temperature, 2);
+
+		return temperature.doubleValue();
+	}
+
+	private int getSensorValue(int index) {
+		int sensorValue = 0;
+
+		try {
+			sensorValue = ik.getSensorValue(index);
+		} catch (PhidgetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return sensorValue;
 	}
 
 	/**
@@ -152,6 +211,24 @@ public class SensorThing extends VirtualThing {
 			throws Exception {
 		setProperty(propertyName, getProperty(propertyName)
 				.getPropertyDefinition().getDefaultValue().getValue());
+	}
+
+	public static double round(double value, int places) {
+		if (places < 0)
+			throw new IllegalArgumentException();
+
+		BigDecimal bd = new BigDecimal(value);
+		bd = bd.setScale(places, RoundingMode.HALF_UP);
+		return bd.doubleValue();
+	}
+
+	public static BigDecimal round(BigDecimal value, int places) {
+		if (places < 0)
+			throw new IllegalArgumentException();
+
+		BigDecimal bd = value;
+		bd = bd.setScale(places, RoundingMode.HALF_UP);
+		return bd;
 	}
 
 }
